036c2ed4e70e689adf0a9f0f881034f58aa2f0d4
vagrant <vagrant@homestead>
2017-05-22 16:44:30 +0000

    service provider

---
+++ src/Backform/BackFormServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Backform\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class BackformServiceProvider extends ServiceProvider
+{
+}
---
+++ src/Backform/Backform.php
@@ -0,0 +1,10 @@
+<?php
+namespace Backform;
+
+class Backform{
+	public $propriedade = 1;
+	public $propriedade = 2;
+	public $propriedade = 3;
+}
+
+?>
